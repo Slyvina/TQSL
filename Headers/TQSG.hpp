40bb527b9d9621d2bd8513d3a7f25c71897397ff
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-16 21:35:14 +0100

    Header start

--- Headers/TQSG.hpp
+++ Headers/TQSG.hpp
@@ -3,6 +3,16 @@
 #include <JCR6_Core.hpp>
 
 namespace Slyvina {
-namespace TQSG{
-}
+	namespace TQSG {
+
+		class _____TIMAGE; // NEVER USE THIS TYPE DIRECTLY! ONLY USE 'TImage' or 'TUImage' in stead, or you'll regret it!!!
+
+		typedef std::shared_ptr<_____TIMAGE> TImage; // A shared pointer to use for images.
+		typedef std::unique_ptr<_____TIMAGE> TUImage; // A unique pointer to use for images.
+
+
+		class _____TIMAGE {
+
+		};
+	}
 }
