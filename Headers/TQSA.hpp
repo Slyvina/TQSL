f37348a8416f1fdf86555ea3179370643781fdb3
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-18 22:56:31 +0100

    TQSA

--- Headers/TQSA.hpp
+++ Headers/TQSA.hpp
@@ -17,11 +17,53 @@
 // misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
+
 #pragma once
+#include <SDL.h>
+#include <SDL_mixer.h>
 #include <Slyvina.hpp>
 #include <JCR6_Core.hpp>
 
 namespace Slyvina {
-namespace TQSA{
-}
+	namespace TQSA {
+		class _____TAudio;
+		typedef std::shared_ptr<_____TAudio> TAudio; // Shared pointer holding audio
+		typedef std::unique_ptr<_____TAudio> TUAudio; // Unique pointer holding audio
+
+		class _____TAudio {
+		private:
+			Mix_Chunk* ActualChunk = nullptr;
+			/// <summary>
+			/// Disposes the sound chunk attacked from the memory (would happen automatically if the destructor is called)
+			/// </summary>
+			///	void Kill();
+			static uint64 count_audio;
+			uint64 _id{ ++count_audio }; // debug only!
+		public:
+
+			_____TAudio(const char* File);
+			_____TAudio(JCR6::JT_Dir JCRResource, std::string JCREntry);
+			//_____TAudio(std::string JCRMain, std::string JCREntry);
+
+			int Play(int loops = 0);
+			void ChPlay(int channel, int loops = 0);
+
+			//bool HasChunk();
+			//bool AutoKill{ true }; // Use with CARE!
+			~_____TAudio();
+		};
+
+		bool Init_TQSA(int demandflags=0);
+
+		std::string AudioError();
+
+		TAudio LoadAudio(std::string File);
+		TUAudio LoadUAudio(std::string File);
+		TAudio LoadAudio(JCR6::JT_Dir JCRResource, std::string Entry);
+		TUAudio LoadUAudio(JCR6::JT_Dir JCRResource, std::string Entry);
+		TAudio LoadAudio(std::string JCRResource, std::string Entry);
+		TUAudio LoadUAudio(std::string JCRResource, std::string Entry);
+
+	}
+
 }
