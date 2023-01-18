23a7483bdf94d15bfdee8ed48280aa0cfccabee5
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-01-18 21:13:41 +0100

    vUpdate

--- Headers/TQSE.hpp
+++ Headers/TQSE.hpp
@@ -1,8 +1,8 @@
 // Lic:
 // TQSL/Headers/TQSE.hpp
 // Tricky's Quick SDL Event Handler (header)
-// version: 22.12.18
-// Copyright (C) 2022 Jeroen P. Broks
+// version: 23.01.17
+// Copyright (C) 2022, 2023 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
--- Headers/TQSG.hpp
+++ Headers/TQSG.hpp
@@ -1,8 +1,8 @@
 // Lic:
 // TQSL/Headers/TQSG.hpp
 // Tricky's Quick SDL2 Graphics (header)
-// version: 22.12.19
-// Copyright (C) 2022 Jeroen P. Broks
+// version: 23.01.17
+// Copyright (C) 2022, 2023 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
