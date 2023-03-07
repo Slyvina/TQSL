6a00a4a682a5e92fe919f78f2a11a725dd52c428
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-03-07 13:09:39 +0100

    vUpdate

--- Source/TQSG.cpp
+++ Source/TQSG.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // TQSL/Source/TQSG.cpp
 // Tricky's Quick SDL2 Graphics
-// version: 23.01.17
+// version: 23.03.06
 // Copyright (C) 2022, 2023 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
