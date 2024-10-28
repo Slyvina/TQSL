8719d0d6ffbcb0505d1228896ac4f9956c168659
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-28 22:47:01 +0100

    Void: VStudio vs GCC conflict

--- Source/TQSG.cpp
+++ Source/TQSG.cpp
@@ -1,3 +1,26 @@
+// License:
+// 	TQSL/Source/TQSG.cpp
+// 	Tricky's Quick SDL2 Graphics
+// 	version: 24.10.28
+// 
+// 	Copyright (C) 2022, 2023, 2024 Jeroen P. Broks
+// 
+// 	This software is provided 'as-is', without any express or implied
+// 	warranty.  In no event will the authors be held liable for any damages
+// 	arising from the use of this software.
+// 
+// 	Permission is granted to anyone to use this software for any purpose,
+// 	including commercial applications, and to alter it and redistribute it
+// 	freely, subject to the following restrictions:
+// 
+// 	1. The origin of this software must not be misrepresented; you must not
+// 	   claim that you wrote the original software. If you use this software
+// 	   in a product, an acknowledgment in the product documentation would be
+// 	   appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be
+// 	   misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
+// End License
 // Lic:
 // TQSL/Source/TQSG.cpp
 // Tricky's Quick SDL2 Graphics
@@ -836,9 +859,12 @@ namespace Slyvina {
 			auto _scaley{ TQSG::_scaley };
 			_LastError = "";
 			if (frame < 0 || frame >= Textures.size()) {
+				/* Old code, and gcc doesn't like sprintf_s
 				char FE[400];
 				sprintf_s(FE, 395, "XDRAW:Texture frame assignment out of bouds! (%d/%d)", frame, (int)Textures.size());
 				_LastError = FE;
+				*/
+				auto FE = TrSPrintF("XDRAW:Texture frame assignment out of bouds! (%d/%d)", frame, (int)Textures.size());
 				Paniek(FE);
 				return;
 			}
