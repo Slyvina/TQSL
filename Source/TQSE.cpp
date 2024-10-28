756cf62e1755412c24796a3e4f6405cffdd96f8f
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-28 22:43:33 +0100

    Fix: Typo

--- Source/TQSE.cpp
+++ Source/TQSE.cpp
@@ -1,22 +1,26 @@
-// Lic:
-// TQSL/Source/TQSE.cpp
-// Slyvina - Tricky's Quick SDL2 Event handler
-// version: 23.10.08
-// Copyright (C) 2022, 2023 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
+// License:
+// 	TQSL/Source/TQSE.cpp
+// 	Slyvina - Tricky's Quick SDL2 Event handler
+// 	version: 24.10.28
+// 
+// 	Copyright (C) 2022, 2023, 2024 Jeroen P. Broks
+// 
+// 	This software is provided 'as-is', without any express or implied
+// 	warranty.  In no event will the authors be held liable for any damages
+// 	arising from the use of this software.
+// 
+// 	Permission is granted to anyone to use this software for any purpose,
+// 	including commercial applications, and to alter it and redistribute it
+// 	freely, subject to the following restrictions:
+// 
+// 	1. The origin of this software must not be misrepresented; you must not
+// 	   claim that you wrote the original software. If you use this software
+// 	   in a product, an acknowledgment in the product documentation would be
+// 	   appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be
+// 	   misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
+// End License
 
 #include <TQSE.hpp>
 #include <SlyvString.hpp>
@@ -607,7 +611,7 @@ namespace Slyvina {
 					/* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
 					{   0,   0, 0 },
 					/* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
-					{ 255,   2550, 0 }
+					{ 255,   255, 0 }
 				}
 			};
 			const SDL_MessageBoxData messageboxdata = {
